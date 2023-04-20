fd405cd6a260b69f90550d23a33ceac51eb71fbe
JoonHo Son <joonho.son@me.com>
2023-04-21 00:04:53 +0900

    variables project 수정

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,35 @@
 fn main() {
-    let x = 5;
+    /*let x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+    */
+
+    /*
+    let mut x = 5;
+
+    println!("The value of x is : {}", x);
+
+    x = 6;
+
+    println!("The value of x is : {}", x);
+    */
+
+    let x = 5;
+
+    let x = x + 1; // 6
+
+    let x = x * 2; // 12
+    
+    println!("The value of x is : {}", x);
+
+    //
+    // mismatched types error
+    // expected `&str`, found `usize`
+    //
+
+    /*
+    let mut spaces = "      ";
+    spaces = spaces.len();
+    */
 }
