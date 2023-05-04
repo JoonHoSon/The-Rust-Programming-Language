a2158e7fa3906d393fa242f2e1517ef043d6d0e0
JoonHo Son <joonho.son@me.com>
2023-05-04 10:43:05 +0900

    feat : 7.3 use로 이름 가져오기
    - communicator library의 test 수정
    - 문서상의 ::client::connect()는 동작하지 않음

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -4,11 +4,11 @@ pub mod network;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::client;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        // ::client::connect() 동작하지 않음
+        client::connect();
     }
 }
