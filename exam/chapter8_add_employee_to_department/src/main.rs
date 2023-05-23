d9f33cdbe065d986f8ece7c5ff8ef8ded9528175
JoonHo Son <joonho.son@me.com>
2023-05-23 22:10:38 +0900

    문제에서 제시한 방식으로 변경 중

--- exam/chapter8_add_employee_to_department/src/main.rs
+++ exam/chapter8_add_employee_to_department/src/main.rs
@@ -1,24 +1,19 @@
 //! 명령어 종류
-//! * add : 직원을 특정 부서에 등록
-//! * show : 전체 혹은 특정 부서의 직원 목록 출력(이름 정렬)
-//! * 명령어 목록 화면으로 이동
-//! * quit : 프로그램 종료
+//! * add John to engineering
+//! * show engineering
+//! * show all
+//! * quit
 
 use std::collections::HashMap;
 use std::io;
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 const MESSAGE: &str = "
-Please select command.
+Please enter command
 ex1) add John to engineering
 ex2) show all
 ex3) show engineering
 ex4) exit
-";
-const SHOW_MESSAGE: &str = "
-department : Show departments.
-[department name] : Show employees of [department name].
-main : Move to main command scree.
 ";
 
 fn main() {
@@ -32,35 +27,48 @@ fn main() {
 
         io::stdin()
             .read_line(&mut input)
-            .expect("Input [add | show | main | quit]");
+            .expect(&format!("{}{}", CLEAR, MESSAGE)[..]);
 
         let command = input.trim().to_lowercase();
+        let split_words: Vec<&str> = command.split(" ").collect();
+        let first_word: &str = split_words.get(0).expect(&format!("{}{}", CLEAR, MESSAGE)[..]);
 
-        if command == "add" {
-        } else if command == "show" {
-            show_command();
-        } else if command == "main" {
-            display_command();
-        } else if command == "exit" {
-            break;
-        } else {
-            println!("{}", "Input [add | show]");
+        match first_word {
+            "add" => {
 
-            continue;
+            },
+            "show" => {
+
+            },
+            "exit" => {
+                break;
+            }
+            _ => println!("{}{}", CLEAR, MESSAGE),
+
         }
 
-        current_command = command;
+
+
+        // if command == "add" {
+        // } else if command == "show" {
+        //     show_command();
+        // } else if command == "main" {
+        //     display_command();
+        // } else if command == "exit" {
+        //     break;
+        // } else {
+        //     println!("{}", "Input [add | show]");
+        //
+        //     continue;
+        // }
+        //
+        // current_command = command;
     }
 }
 
 fn display_command() {
     println!("{}", CLEAR);
     println!("{}", MESSAGE);
-}
-
-fn show_command() {
-    println!("{}", CLEAR);
-    println!("{}", SHOW_MESSAGE);
 }
 
 fn show_department_list(map: &HashMap<String, String>) {
